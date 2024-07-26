77a69e3fa75b479a6927282f9d28c2392374deff
Rasheed <rasheedbabatunde76@gmail.com>
2024-07-26 11:27:37 +0100

    deploy

--- ecommerce_core/settings.py
+++ ecommerce_core/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 from datetime import timedelta
@@ -20,12 +22,12 @@ from datetime import timedelta
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-a$wvb_!3plmd+%*jztw+is-=3o%h61y!xju&g_z-tl0clp3g+d"
 
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ecommerce-core.onrender.com' , 'localhost' , '127.0.0.1']
 
 AUTH_USER_MODEL = 'accounts.User'
 
@@ -123,6 +125,10 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.parse(config('DATABASE_URL'))
+# }
 
 
 REST_FRAMEWORK = {
@@ -234,10 +240,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_USE_TLS = True
-EMAIL_USE_SSL = True
-EMAIL_PORT = 465
-# EMAIL_PORT = 587
-EMAIL_HOST_USER = 'brasheed240@gmail.com'
-EMAIL_HOST_PASSWORD = 'vhghnszeyyqmxohc'
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_PORT = config("EMAIL_PORT")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_USE_SSL = config("EMAIL_USE_SSL")
